0b3e70a9175eb2f68326a33532d08eb034a42727
Rafael Acioly <aciolyr@gmail.com>
2019-11-23 11:14:09 -0300

    adds custom exception message

--- pybreaker/exceptions.py
+++ pybreaker/exceptions.py
@@ -1,2 +1,16 @@
+from inspect import signature
+from datetime import datetime
+
+
 class CircuitBreakerOpen(Exception):
-    pass
+
+    def __init__(self, method):
+        self.method = method
+
+    def __str__(self):
+        full_date = datetime.today().strftime("%D %H:%M:%S")
+        return "[{full_date}] - circuit breaker is open " \
+                "for method \"{name}\" ".format(
+                    full_date=full_date,
+                    name=self.method.__name__
+                )
