27cc1d0e35061749e65da6c9f6c26577199f703d
Rafael Acioly <aciolyr@gmail.com>
2019-11-30 11:07:25 -0300

    adds description to setup file

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from distutils.core import setup
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='circuitpy',
     version='0.0.1',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Rafael Acioly',
     author_email='aciolyr@gmail.com',
     packages=['circuitpy'],
     description='A simple circuit breaker implementation',
-    keyworkds=['circuit breaker'],
+    keyworkds=['circuit', 'breaker', 'integration'],
     license="MIT",
     url='https://github.com/rafa-acioly/circuitpy',
-    download_url='https://github.com/rafa-acioly/circuitpy'
+    download_url='https://github.com/rafa-acioly/circuitpy',
+    test_suite='tests'
 )
